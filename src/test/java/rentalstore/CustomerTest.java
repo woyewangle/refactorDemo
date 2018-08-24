5281311e84a21bc409b3fb591f43639b2a3dee5b
LEON YU (ITA-ISDC-ISD-OOCLL/ZHA) <YULE4@OOCL.COM>
2018-08-24 14:24:55 +0800

    [Leon]add Customer test case to test statement that  given a customer who has rent one movie for 1 day

--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -21,6 +21,23 @@ public class CustomerTest {
         assertEquals("Rental Record for Jerry\nAmount owed is 0.0\nYou earned 0 frequent renter points", statement);
     }
 
+    @Test
+    public void should_return_correct_statement_given_customer_has_rent_one_regular_movie_for_1_day() {
+        Movie regularMovie = new Movie("Titanic", 0);
+        Rental oneDayRental = new Rental(regularMovie, 1);
+        customer.addRental(oneDayRental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Jerry\n" +
+                "\t" + regularMovie.getTitle() + "\t2.0\n" +
+                "Amount owed is 2.0\n" +
+                "You earned 1 frequent renter points", statement);
+    }
+
+
+
+
 
 
 
