1a2981b198eb938eb22b8c3bb4545d0173eab6d3
“Leon <leon.yu@oocl.com>
2018-08-28 00:07:01 +0800

    [Leon] add test for call statement given one rental which movie is new

--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -2,6 +2,7 @@ package rentalstore;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 /**
@@ -28,12 +29,40 @@ public class CustomerTest {
         customer.addRental(oneDayRental);
 
         String statement = customer.statement();
-
         assertEquals("Rental Record for Jerry\n" +
                 "\t" + regularMovie.getTitle() + "\t2.0\n" +
                 "Amount owed is 2.0\n" +
                 "You earned 1 frequent renter points", statement);
     }
+
+    @Test
+    public void should_return_correct_statement_given_customer_has_rent_one_regular_movie_for_3_day() {
+        Movie regularMovie = new Movie("Titanic", 0);
+        Rental oneDayRental = new Rental(regularMovie, 3);
+        customer.addRental(oneDayRental);
+
+        String statement = customer.statement();
+        assertEquals("Rental Record for Jerry\n" +
+                "\t" + regularMovie.getTitle() + "\t3.5\n" +
+                "Amount owed is 3.5\n" +
+                "You earned 1 frequent renter points", statement);
+    }
+
+    @Test
+    public void should_return_correct_statement_given_customer_new_release_movie_has_rent_one_rental_and_day_rented_1() {
+        Movie movie =new Movie("movie1Title",Movie.NEW_RELEASE);
+        Rental rental = new Rental(movie,1);
+        customer.addRental(rental);
+        String result = customer.statement();
+        assertThat(result,is("Rental Record for Jerry\n" +
+                "\tmovie1Title\t3.0\n" +
+                "Amount owed is 3.0\n" +
+                "You earned 1 frequent renter points"));
+    }
+
+
+
+
 
 
 
